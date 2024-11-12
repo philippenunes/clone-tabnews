9e56d3806fe9e1463ee05b196a7a5293a27f5e56
philippenunes <philippenunes@live.com>
2024-11-12 02:47:32 +0000

    modificando frase da Home

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Oi Bruna, te amo, se me ama da uma risadinha :)</h1>
+    return <h1>Oi Bruna, te amo, se me ama da uma risadinha! 😎</h1>
 }
 
 export default Home;
